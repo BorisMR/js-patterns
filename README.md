419cfd6b51b155c8571d94d564ea8d08d573b8d3
BorisMR <anum.bmr.01@gmail.com>
2017-11-18 13:41:22 -0300

    module pattern

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # js-patterns
+
 just coding some js design patterns
+
+Module Pattern: function that contains an execution context
---
+++ module-pattern.js
@@ -0,0 +1,19 @@
+var MyModule = {};
+
+MyModule = (function () {
+	var v1, v2, v3;
+
+	function privateMethodOne() {
+
+	}
+
+	return {
+		publicMethod: function () {
+
+		},
+
+		otherPublicMethod () {
+
+		}
+	}
+}());
